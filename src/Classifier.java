db371f1cae00724fec0c3348fac8a163120cec4e
Marc Juchli <marc@mjnet.ch>
2014-03-10 18:03:01 -0700

    working proof of concept

---
+++ src/Classifier.java
@@ -0,0 +1,124 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by Marc on 09.03.14.
+ */
+public class Classifier {
+
+    public HashMap<String, HashMap<String, Double>> categories = new HashMap<String, HashMap<String, Double>>();
+    private ArrayList<MessageNode> trainMessages = new ArrayList<MessageNode>();
+
+    class MessageNode {
+        public String cat;
+        public ArrayList<String> text;
+        MessageNode(String cat, ArrayList<String> text){
+            this.cat = cat;
+            this.text = text;
+        }
+    }
+
+    public ArrayList<MessageNode> getTrainMessages(){
+        return this.trainMessages;
+    }
+
+    public double calculateProbability(ArrayList<String> message, String category){
+        double probMessageIsCat = getOptimizedProbabilityOfCategory(category);
+
+        for(String word : message){
+            Double occuranceInCategory = categories.get(category).get(word);
+
+            if (occuranceInCategory != null) {
+                probMessageIsCat += Math.log10(occuranceInCategory/wordsPerCategory(category));
+            } else {
+                probMessageIsCat += Math.log10(0.000001);
+            }
+        }
+        return probMessageIsCat;
+    }
+
+
+    public ArrayList<ArrayList<String>> loadTestMessages(BufferedReader input) throws IOException {
+        String line;
+        Tokenizer t = new Tokenizer();
+        ArrayList<ArrayList<String>> messages = new ArrayList<ArrayList<String>>();
+        while((line = input.readLine()) != null){
+            if(line.length() > 0){
+                String textLine = t.clearUglyChars(line);
+                String[] text = textLine.split(" ");
+
+                messages.add(t.clear(text));
+            }
+        }
+        return messages;
+    }
+
+
+    public void loadTrainMessages(BufferedReader input) throws IOException {
+        String line;
+        Tokenizer t = new Tokenizer();
+        while((line = input.readLine()) != null){
+            if(line.length() > 0){
+
+                String[] words = line.split("\t");
+                String category = words[0];
+
+
+                String textLine = t.clearUglyChars(words[1]);
+                String[] text = textLine.split(" ");
+
+                MessageNode message = new MessageNode(category, t.clear(text));
+                getTrainMessages().add(message);
+            }
+        }
+    }
+
+    public void fillCategories(){
+        for(MessageNode n : getTrainMessages()){
+            for(String word : n.text){
+
+                HashMap<String, Double> tuple = categories.get(n.cat);
+                if(tuple == null){
+                    tuple = new HashMap<String, Double>();
+                }
+
+                Double count = tuple.get(word);
+                if(count != null){
+                    tuple.put(word, count+1.0);
+                } else {
+                    tuple.put(word, 1.0);
+                }
+
+                categories.put(n.cat, tuple);
+
+            }
+        }
+    }
+
+    public int numberOfMessages(){
+        return getTrainMessages().size();
+    }
+
+    public int numberOfMessagesPerCategory(String cat){
+        int i = 0;
+        for(MessageNode m : getTrainMessages()){
+            if(m.cat.equals(cat)){
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public int wordsPerCategory(String cat){
+        return categories.get(cat).size();
+    }
+
+    public double getProbabilityOfCategory(String cat){
+        return (double) numberOfMessagesPerCategory(cat)/(double) numberOfMessages();
+    }
+
+    public double getOptimizedProbabilityOfCategory(String cat){
+        return Math.log(getProbabilityOfCategory(cat));
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Marc on 10.03.14.
+ */
+public class Main {
+
+    private static String TrainFile = "smsSpamCollection-train.txt";
+    private static String TestFile = "smsSpamCollection-test-sample.txt";
+    private static String predictionFile = "prediction.txt";
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader trainInput = new BufferedReader(new FileReader(TrainFile));
+            BufferedReader testInput = new BufferedReader(new FileReader(TestFile));
+
+            Classifier c = new Classifier();
+            try {
+                c.loadTrainMessages(trainInput);
+                c.fillCategories();
+
+                System.out.println("Number of Messages: " + c.numberOfMessages());
+                System.out.println("Number of Messages in ham: " + c.numberOfMessagesPerCategory("ham"));
+                System.out.println("Number of Words in ham: " + c.wordsPerCategory("ham"));
+                System.out.println("Probability of ham: " + c.getProbabilityOfCategory("ham"));
+                System.out.println("Optmized (log) Probability of ham: " + c.getOptimizedProbabilityOfCategory("ham"));
+                System.out.println("Number of Messages in spam: " + c.numberOfMessagesPerCategory("spam"));
+                System.out.println("Number of Words in spam: " + c.wordsPerCategory("spam"));
+                System.out.println("Probability of spam: " + c.getProbabilityOfCategory("spam"));
+                System.out.println("Optmized (log) Probability of spam: " + c.getOptimizedProbabilityOfCategory("spam"));
+
+
+                //Test
+                FileWriter write = new FileWriter(predictionFile, false);
+                PrintWriter predictionPrinter = new PrintWriter(write);
+
+                ArrayList<ArrayList<String>> messages = c.loadTestMessages(testInput);
+                for(ArrayList<String> message : messages){
+                    double probMessageIsHam = c.calculateProbability(message, "ham");
+                    double probMessageIsSpam = c.calculateProbability(message, "spam");
+
+                    //then compare probs -> the one with the highest value
+                    if (probMessageIsHam < probMessageIsSpam) {
+                        System.out.println("spam");
+                        predictionPrinter.printf("%s" + "%n", "spam");
+                    } else {
+                        System.out.println("ham");
+                        predictionPrinter.printf("%s" + "%n", "ham");
+                    }
+                }
+
+                predictionPrinter.close();
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
